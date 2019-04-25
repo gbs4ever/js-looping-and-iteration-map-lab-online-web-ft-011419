7256a3c4274f8157c83e7d04985bb145f1daca85
Sim Greenbaum <gbs4ever2@gmail.com>
2019-04-25 18:02:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Code your solution in this file.
-function lowerCaseDrivers(data){return data.map(function(newData)){
+function lowerCaseDrivers(data){
+  return data.map(function(newData){
 return   newdata.toLowerCase()
-}
+})
 
 }
